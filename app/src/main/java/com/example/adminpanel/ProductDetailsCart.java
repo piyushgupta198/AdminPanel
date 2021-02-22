998528928c68db5e807212cb71d7406eab0d2f3a
printIndiaMart <probeyourcaretaker198@gmail.com>
2021-02-22 15:55:39 +0530

    22 feb

---
+++ app/src/main/java/com/example/adminpanel/ProductDetailsCart.java
@@ -0,0 +1,38 @@
+package com.example.adminpanel;
+
+public class ProductDetailsCart {
+
+    String productName;
+    int price;
+    int quantity;
+
+    public ProductDetailsCart(String productName, int price, int quantity) {
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
